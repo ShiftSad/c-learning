de770a1a29ea860c4e70b02b43c7553304f2da1d
Sadness <rosalimavictor@gmail.com>
2025-02-28 13:55:43 -0300

    Ignore spaces

--- better-calculator/better-calculator.c
+++ better-calculator/better-calculator.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
-{
+int main() {
     char input[128];
     printf("Type operation: ");
 
@@ -11,9 +10,11 @@ int main()
     input[strlen(input) - 1] = '\0'; // Remove \n at the last character
 
     // Remove spaces
-    
+    int temp = 0;
+    char operation[sizeof(input)];
     for (int i = 0; i < strlen(input); i++) {
-
+        // 32 is the unicode for space
+        if (input[i] != 32) operation[temp++] = input[i];
     }
 
     return 0;
