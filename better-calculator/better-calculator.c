79f64907865ae49517ae0d02ad377fddc93f6d68
Sadness <rosalimavictor@gmail.com>
2025-02-28 14:17:02 -0300

    Check if braces are valid

--- better-calculator/better-calculator.c
+++ better-calculator/better-calculator.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Thanks to https://stackoverflow.com/questions/71369018/using-c-to-determine-if-a-string-contains-any-imbalanced-brackets
+const char *check_brace(const char str[], int endc) {
+    while (str) {
+        int c = *str++;
+        switch (c) {
+          case '(': str = check_brace(str, ')'); break;
+          case '[': str = check_brace(str, ']'); break;
+          case '{': str = check_brace(str, '}'); break;
+          case ')':
+          case ']':
+          case '}':
+          case '\0': return (c == endc) ? str : NULL;
+        }
+    }
+    return NULL;
+}
+
+int balanced(const char *line) {
+    return check_brace(line, '\0') != NULL;
+}
+
 int main() {
     char input[128];
     printf("Type operation: ");
@@ -16,6 +37,9 @@ int main() {
         // 32 is the unicode for space
         if (input[i] != 32) operation[temp++] = input[i];
     }
+
+    // Check if braces are valid
+    printf("Are braces valid? %i\n", balanced(operation));
 
     return 0;
 }
