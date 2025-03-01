027f5bc4485aa933cdc96b05a0d28523591b7491
Sadness <rosalimavictor@gmail.com>
2025-03-01 01:15:12 -0300

    Won't even pretend it was my code. This is 100% ai generated. I won't be bothering about C cross platform in my first project, at 1:14 AM on an saturnday

--- approximate-pi/pi.c
+++ approximate-pi/pi.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include <pthread.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include <process.h> // For _beginthreadex
+    typedef HANDLE thread_t;
+#else
+    #include <pthread.h>
+    typedef pthread_t thread_t;
+#endif
 
 typedef long double (*piFunc)(int);
 
@@ -12,15 +20,26 @@ typedef struct {
     int n;
 } ThreadData;
 
-// Thanks chatgpt
+// Get wall time in seconds (cross-platform)
 double get_wall_time() {
-    struct timespec ts;
-    clock_gettime(1, &ts);
-    return ts.tv_sec + ts.tv_nsec / 1e9;
+    #ifdef _WIN32
+        LARGE_INTEGER time, freq;
+        QueryPerformanceFrequency(&freq);
+        QueryPerformanceCounter(&time);
+        return (double)time.QuadPart / freq.QuadPart;
+    #else
+        struct timespec ts;
+        clock_gettime(1, &ts);
+        return ts.tv_sec + ts.tv_nsec / 1e9;
+    #endif
 }
-
+    
 // https://stackoverflow.com/questions/74210984/how-to-achieve-parallelism-in-c :)
+#ifdef _WIN32
+unsigned __stdcall thread_function(void *arg) {
+#else
 void *thread_function(void *arg) {
+#endif
     ThreadData *data = (ThreadData *)arg;
     double start = get_wall_time();
     long double result = data->func(data->n);
@@ -28,7 +47,12 @@ void *thread_function(void *arg) {
     double elapsed = end - start;
 
     printf("%s >> %.8Lf (Elapsed time: %.5f seconds)\n", data->methodName, result, elapsed);
+#ifdef _WIN32
+    _endthreadex(0);
+    return 0;
+#else
     return NULL;
+#endif
 }
 
 // Thanks https://en.wikipedia.org/wiki/Pi :)
@@ -64,12 +88,23 @@ int main() {
     ThreadData data1 = {"Gregory Leibniz", gregoryLeibnizSeries, n};
     ThreadData data2 = {"Basel Problem", baselProblem, n};
 
+#ifdef _WIN32
+    threads[0] = (HANDLE)_beginthreadex(NULL, 0, thread_function, &data1, 0, NULL);
+    threads[1] = (HANDLE)_beginthreadex(NULL, 0, thread_function, &data2, 0, NULL);
+
+    // Wait for both threads to finish
+    WaitForMultipleObjects(2, threads, TRUE, INFINITE);
+
+    CloseHandle(threads[0]);
+    CloseHandle(threads[1]);
+#else
     pthread_create(&threads[0], NULL, thread_function, &data1);
     pthread_create(&threads[1], NULL, thread_function, &data2);
 
-    // Wait for both threads to finish.
+    // Wait for both threads to finish
     pthread_join(threads[0], NULL);
     pthread_join(threads[1], NULL);
+#endif
 
     return 0;
 }
