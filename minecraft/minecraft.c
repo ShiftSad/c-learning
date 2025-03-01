735cd941655b1eea15736454216b0c2a88eec635
Sadness <rosalimavictor@gmail.com>
2025-03-01 15:21:57 -0300

    Allow for multiple simultaneos connections, thanks to ChatGPT :)

--- minecraft/minecraft.c
+++ minecraft/minecraft.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +8,31 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-// Thanks GeeksForGeeks https://www.geeksforgeeks.org/socket-programming-cc/
+void *handle_client(void *socket_desc) {
+    int client_socket = *(int *)socket_desc;
+    free(socket_desc);
+
+    char buffer[BUFFER_SIZE] = {0};
+    while (1) {
+        int valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Client disconnected.\n");
+            break;
+        }
+
+        printf("Received: %s\n", buffer);
+        send(client_socket, buffer, valread, 0); // Echo back
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+
+    close(client_socket);
+    return NULL;
+}
+
 int main() {
     int server_fd;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == 0) {
@@ -17,14 +40,13 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Make port immediatly reusable after restarting
+    // Make port immediately reusable after restarting
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("Error setting socket options");
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY; // Any network interface
     address.sin_port = htons(PORT);       // Convert port to network byte order
@@ -35,34 +57,33 @@ int main() {
     }
 
     // Listen for incoming connections
-    // 3 is the size of the queue
-    if (listen(server_fd, 3) < 0) { // returns 0 on success and -1 on failure
+    // 10 is the size of the queue
+    if (listen(server_fd, 10) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", PORT);
 
-    int new_socket;
-    socklen_t addrlen = sizeof(address);
-    new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
-    if (new_socket < 0) {
-        perror("Accept failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Connection established.\n");
-
-    char buffer[BUFFER_SIZE] = { 0 };
     while (1) {
-        int valread = read(new_socket, buffer, BUFFER_SIZE);
-        if (valread <= 0) {
-            printf("Client disconnected.\n");
-            break;
+        int *new_socket = malloc(sizeof(int)); // Allocate memory for each client socket
+        *new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (*new_socket < 0) {
+            perror("Accept failed");
+            free(new_socket);
+            continue;
         }
 
-        printf("Received: %s\n", buffer);
-        send(new_socket, buffer, valread, 0); // Echo back
-        memset(buffer, 0, BUFFER_SIZE);
+        printf("New client connected.\n");
+
+        pthread_t client_thread;
+        if (pthread_create(&client_thread, NULL, handle_client, new_socket) != 0) {
+            perror("Could not create thread");
+            free(new_socket);
+        }
+        pthread_detach(client_thread); // Automatically cleans up thread resources
     }
+
+    close(server_fd);
+    return 0;
 }
